bafeef10cd21bcc83f953dbaf4a4a63a4405764a
nibru <nikolaus.brunner@protonmail.ch>
2020-12-25 17:04:11 +0100

    Removed react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,14 @@
-import { QueryCache, ReactQueryCacheProvider } from 'react-query';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 
 import '../styles/globals.scss';
 
-const queryCache = new QueryCache();
-
 library.add(fab, fas);
 
 const App = ({ Component, pageProps }) => (
   <>
-    <ReactQueryCacheProvider queryCache={queryCache}>
-      <Component {...pageProps} />
-    </ReactQueryCacheProvider>
+    <Component {...pageProps} />
   </>
 );
 
