0c33613397c5cd781fdd1c51e683822c1d8d7a66
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2021-04-06 17:36:20 +0200

    added breakpoints ts file

---
+++ lib/breakpoints.ts
@@ -0,0 +1,19 @@
+export interface Breakpoints {
+  xs: string;
+  s: string;
+  m: string;
+  l: string;
+  xl: string;
+  xxl: string;
+  xxxl: string;
+}
+
+export const breakpoints: Breakpoints = {
+  xs: '320px',
+  s: '414px',
+  m: '768px',
+  l: '1024px',
+  xl: '1280px',
+  xxl: '1440px',
+  xxxl: '1920px'
+};
