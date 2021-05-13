e76292a01dd422c75a13cf82c19867a8e297542e
nibru <nikolaus.brunner@protonmail.ch>
2021-05-13 16:36:52 +0200

    Ran format and style

--- data/projects.ts
+++ data/projects.ts
@@ -4,8 +4,8 @@ const projects: Project[] = [
   {
     title: 'BlubbiProject',
     description:
-      '# Header 1\n\nDescription\n\n## Header 2\n\n- One\n- Two \n- Three',
-  },
+      '# Header 1\n\nDescription\n\n## Header 2\n\n- One\n- Two \n- Three'
+  }
 ];
 
 export default projects;
