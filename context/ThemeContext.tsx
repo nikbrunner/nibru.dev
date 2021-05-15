60d48378fdcb294370dd91875ee9c2e634beeaf7
nibru <nikolaus.brunner@protonmail.ch>
2021-05-15 16:46:26 +0200

    Updated ThemeContext

--- context/ThemeContext.tsx
+++ context/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 export enum Theme {
   dark = 'dark',
@@ -20,6 +20,50 @@ interface Props {
 
 export const ThemeProvider = ({ children }: Props) => {
   const [theme, setTheme] = useState<Theme>(Theme.dark);
+
+  // Listen for preferred color scheme
+  useEffect(() => {
+    const prefersDarkMode: MediaQueryList = window.matchMedia(
+      '(prefers-color-scheme: dark)'
+    );
+
+    prefersDarkMode.addEventListener(
+      'change',
+      (event: MediaQueryListEvent): void => {
+        if (event.matches) {
+          setTheme(Theme.dark);
+        } else if (!event.matches) {
+          setTheme(Theme.light);
+        }
+      }
+    );
+  }, []);
+
+  // Read local storage
+  useEffect(() => {
+    if (localStorage.getItem('theme') === Theme.dark) {
+      setTheme(Theme.dark);
+    } else if (localStorage.getItem('theme') === Theme.light) {
+      setTheme(Theme.light);
+    } else if (localStorage.getItem('theme') === null) {
+      setTheme(Theme.dark);
+    }
+  }, []);
+
+  // Write localStorage & set class to html element
+  useEffect(() => {
+    const html = document.documentElement;
+
+    if (theme === Theme.dark) {
+      localStorage.setItem('theme', Theme.dark);
+      html.classList.add(Theme.dark);
+      html.classList.remove(Theme.light);
+    } else if (theme === Theme.light) {
+      localStorage.setItem('theme', Theme.light);
+      html.classList.add(Theme.light);
+      html.classList.remove(Theme.dark);
+    }
+  }, [theme]);
 
   return (
     <ThemeContext.Provider
