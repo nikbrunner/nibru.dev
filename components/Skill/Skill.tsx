721862f9ca66bb0ec046135e9e62c10bfccd334e
nibru <nikolaus.brunner@protonmail.ch>
2021-05-16 19:48:29 +0200

    Removed useAccentColor

--- components/Skill/Skill.tsx
+++ components/Skill/Skill.tsx
@@ -8,22 +8,20 @@ import { GenericProps } from '../../types/GenericProps';
 import { Skill as SkillType } from '../../types/Skill';
 
 import { cn } from '../../lib/cn';
-import { useAccentColor } from '../../lib/useAccentColor';
 
 type SkillProps = SkillType & GenericProps;
 
 const Skill = ({ classNames, title, level, icon }: SkillProps) => {
   const { theme } = useContext(ThemeContext);
-  const accentColor = useAccentColor();
-
-  const skillLevelStyle: React.CSSProperties = {
-    background: accentColor,
-    width: `${level}%`
-  };
 
   return (
     <div className={cn('Skill', classNames, { [theme]: theme })}>
-      <div className='Skill__level' style={skillLevelStyle} />
+      <div
+        className='Skill__level'
+        style={{
+          width: `${level}%`
+        }}
+      />
 
       <div className='Skill__caption'>
         <div className='Skill__icon'>{icon}</div>
