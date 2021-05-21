cdeb13bf7093ee95aa0d30ad473a29d1c7a388ae
nibru <nikolaus.brunner@protonmail.ch>
2021-05-21 19:13:06 +0200

    profile image

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,74 +12,82 @@ const Home = () => {
   return (
     <Page name='Home' title='Hi there. 👋'>
       <Section classNames={['Home__intro']}>
-        <Headline tag='h1' size='h1' noBorder>
-          Hi there.
-        </Headline>
+        <div className='Home__intro-image'>
+          <img src='/images/nb.jpg' />
+        </div>
 
-        <Text maxWidth='25rem'>
-          My name is Nikolaus Brunner,
-          <br />
-          or short just <em>Nik</em>.
-        </Text>
+        <div className='Home__intro-text'>
+          <Headline tag='h1' size='h1' noBorder>
+            Hi there.
+          </Headline>
 
-        <Text maxWidth='30rem'>
-          I am working as a Software Engineer
-          <br /> for{' '}
-          <Link href='https://bike.center' type='external'>
-            DealerCenter Digital
-          </Link>
-          .
-          <br /> Previously I worked for{' '}
-          <Link href='https://https://www.diva-e.com/de/' type='external'>
-            diva-e
-          </Link>
-          .
-        </Text>
+          <Text maxWidth='25rem'>
+            My name is Nikolaus Brunner,
+            <br />
+            or short just <em>Nik</em>.
+          </Text>
 
-        <Text maxWidth='25rem'>
-          I mainly work with <strong>React</strong>, <br />
-          <strong>TypeScript</strong> and <strong>SCSS</strong>.
-        </Text>
+          <Text maxWidth='30rem'>
+            I am working as a Software Engineer for the{' '}
+            <Link href='https://bike.center' type='external'>
+              DealerCenter Digital
+            </Link>
+            .
+            <br /> Previously I worked for{' '}
+            <Link
+              href='https://https://www.diva-e.com/de/'
+              type='external'
+            >
+              diva-e
+            </Link>
+            .
+          </Text>
 
-        <Text maxWidth='30rem'>
-          This website is in the process of being rebuilt.
-          <br /> You can visit my old website{' '}
-          <Link href='/archive/index.html' type='internal'>
-            here
-          </Link>
-          .
-        </Text>
+          <Text maxWidth='25rem'>
+            I mainly work with <strong>React</strong>, <wbr />
+            <strong>TypeScript</strong> and <strong>SCSS</strong>.
+          </Text>
 
-        <Text style={{ fontSize: '2rem' }}>
-          <Link
-            href='https://github.com/nikbrunner'
-            type='external'
-            hideIcon
-          >
-            <FiGithub />
-          </Link>{' '}
-          <Link
-            href='mailto:nikolaus.brunner@protonmail.ch'
-            type='external'
-            hideIcon
-          >
-            <FiMail />
-          </Link>{' '}
-          <Link
-            href='https://www.xing.com/profile/Nikolaus_Brunner4/cv'
-            type='external'
-            hideIcon
-          >
-            <RiXingLine />
-          </Link>{' '}
-          <Link
-            href='https://www.linkedin.com/in/nikolaus-brunner-30847798/'
-            type='external'
-            hideIcon
-          >
-            <FiLinkedin />
-          </Link>
-        </Text>
+          <Text maxWidth='30rem'>
+            This website is being rebuilt.
+            <br /> You can visit my old website{' '}
+            <Link href='/archive/index.html' type='internal'>
+              here
+            </Link>
+            .
+          </Text>
+
+          <Text style={{ fontSize: '2rem' }}>
+            <Link
+              href='https://github.com/nikbrunner'
+              type='external'
+              hideIcon
+            >
+              <FiGithub />
+            </Link>{' '}
+            <Link
+              href='mailto:nikolaus.brunner@protonmail.ch'
+              type='external'
+              hideIcon
+            >
+              <FiMail />
+            </Link>{' '}
+            <Link
+              href='https://www.xing.com/profile/Nikolaus_Brunner4/cv'
+              type='external'
+              hideIcon
+            >
+              <RiXingLine />
+            </Link>{' '}
+            <Link
+              href='https://www.linkedin.com/in/nikolaus-brunner-30847798/'
+              type='external'
+              hideIcon
+            >
+              <FiLinkedin />
+            </Link>
+          </Text>
+        </div>
       </Section>
     </Page>
   );
