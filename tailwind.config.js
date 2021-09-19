80a8d12d4bd15a4af10836371e328f8cfb7d0cd4
nibru <nikolaus.brunner@protonmail.ch>
2021-09-19 22:07:27 +0200

    Updated global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ module.exports = {
   ],
   darkMode: "media", // or 'media' or 'class'
   theme: {
+    fontFamily: {},
+    fontWeight: {
+      hairline: 100,
+      thin: 200,
+      light: 300,
+      normal: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700,
+      extrabold: 800,
+      black: 900
+    },
     extend: {}
   },
   variants: {
