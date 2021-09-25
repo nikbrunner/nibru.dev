ac29740516a0e010e96460fc0019c64ecd939f15
nibru <nikolaus.brunner@protonmail.ch>
2021-09-26 00:03:27 +0200

    Removed redundant files

--- typings/ResponsiveStyles.ts
+++
@@ -1,8 +0,0 @@
-import { IBreakpoints } from "./Breakpoints";
-
-// TODO Connect this type to CSSProperties and Breakpoints somehow
-export type TDynamicCSSProperty<T> = T[];
-
-export type TResponsiveStyle<CSSProperty> = {
-  [Breakpoint in keyof IBreakpoints]?: CSSProperty;
-};
