1f3e002a1afd8a374debdf31eb8f31cec83a890e
nibru <nikolaus.brunner@protonmail.ch>
2021-09-25 22:40:08 +0200

    Added label to BookLayout.style.tsx

--- styles/BookLayout.style.tsx
+++ styles/BookLayout.style.tsx
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 
 export const SBooksLayout = styled.div`
+  label: BooksLayout;
   display: grid;
   gap: 2rem;
   grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
