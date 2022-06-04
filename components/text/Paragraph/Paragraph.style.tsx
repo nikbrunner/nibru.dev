d3b0b26ec2c40999278fe5d3ab5ee3ce36d0ba90
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-06-04 16:50:14 +0200

    Paragraph: set line-height

--- components/text/Paragraph/Paragraph.style.tsx
+++ components/text/Paragraph/Paragraph.style.tsx
@@ -7,6 +7,7 @@ import { space } from "@theme/mixins/space";
 const Style = styled.p<Partial<ComponentProps<typeof Paragraph>>>`
   label: Paragraph;
   padding-bottom: ${space.m};
+  line-height: 1.5;
 `;
 
 export default Style;
